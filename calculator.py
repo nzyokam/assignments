9ca34ab7c806bdd487d7f25f4f4de2a9da06c13f
nzyokam <muusinzyoka@Euphoria.local>
2025-03-16 11:29:26 +0300

    first commit

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def addNumbers(a, b):
+    return a + b
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = addNumbers(a, b)
+print("Sum of the numbers is: ", sum)
+
